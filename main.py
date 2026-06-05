f02c58ffa80a9a6b278aaa14df0d9631bf054551
jyothsnap51-web <jyothsnap51@gmail.com>
2026-06-05 22:33:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+def cur(r):
+    return 2*3.14*r
+radius=int(input("enter the radius of the circle that you want to find the curcumference of :  "))
+print ("curcumference of the circle is-",cur(radius))
